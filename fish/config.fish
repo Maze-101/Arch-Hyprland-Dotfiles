0e6967e9a13f770978fbb70e1452a01ac35e9ae1
shikigami <mazenkassem101@gmail.com>
2026-05-11 06:11:24 +0300

    adding an alias

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,6 @@ end
 alias f='fastfetch'
 alias c='code'
 alias gc='git commit'
+alias s='sudo pacman -S'
 alias lang='setxkbmap -layout us,ara -option grp:alt_shift_toggle'
 starship init fish | source
